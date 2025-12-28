36345af7c65eac3d43d6043c88c1d49344eb797f
rohit-sanwariya <rohitsanwariya1995@gmail.com>
2025-12-28 12:28:23 +0530

    region updated

---
+++ provider.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+  region = "ap-south-1"
+  # You can set access_key and secret_key here or use environment variables
+  # access_key = "YOUR_ACCESS_KEY"
+  # secret_key = "YOUR_SECRET_KEY"
+}
